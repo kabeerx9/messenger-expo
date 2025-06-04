a116e1db37f8c1dbf387e440f75cc021584f32aa
kabeerx9 <kabeer786joshi@gmail.com>
2025-06-04 21:49:23 +0530

    react-native-size-matters installed

--- src/app/(auth)/index.tsx
+++ src/app/(auth)/index.tsx
@@ -1,12 +1,48 @@
-import { Text } from "react-native";
+import imagePath from "@/src/constants/imagePath";
+import { Image, StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const AuthIndexScreen = () => {
     return (
-        <SafeAreaView style={{ flex: 1 }}>
-            <Text>This isn the index screen</Text>
+        <SafeAreaView style={styles.container}>
+            <View style={styles.header}></View>
+            <View style={styles.body} >
+                <Image source={imagePath.react_logo} style={styles.logo} />
+                 </View>
+            <View style={styles.footer} >
+                <Text style={styles.from_text}>From</Text>
+                <Text style={styles.facebook_text}>Facebook</Text>
+            </View>
         </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    container : {
+        flex : 1,
+        alignItems : "center",
+        justifyContent : "space-between",
+        paddingVertical  : 70,
+    },
+    header : {},
+    body : {},
+    logo : {
+        width : 50,
+        height : 50,
+        resizeMode : "contain",
+        borderRadius : 10,
+    },
+    footer : {
+        alignItems : "center",
+    },
+    from_text : {
+        fontSize : 12,
+        color : "#867373"
+    },
+    facebook_text : {
+        fontSize : 15,
+        color : "black",
+    }
+})
 
 export default AuthIndexScreen;
